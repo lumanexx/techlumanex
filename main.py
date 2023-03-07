a9b77c5ec5fe7bf8454fc7ad1520018fda7beaa3
techlumanex <112830564+lumanexx@users.noreply.github.com>
2023-03-07 22:22:48 +0000

    added template

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,  render_templates
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-  return "ELUU P 78, OBI NWANNEM 79"
+def  hello_world():
+  return render_templates('home.html')
   
 if __name__ == "__main__":
   app.run(host= '0.0.0.0' , debug=True)
